73fd2d839f5d1008295aef5454156c0199e8eb11
Francent Jienarta <francentjie@gmail.com>
2026-02-02 22:14:33 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000115728_francentjienarta/week01/GameStore.kt
+++ src/oop_00000115728_francentjienarta/week01/GameStore.kt
@@ -1,2 +1,8 @@
 package oop_00000115728_francentjienarta.week01
 
+fun main(){
+    val gameTitle: String
+    val price: Int
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
