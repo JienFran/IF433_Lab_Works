3faf62310a99a253397085d688af042b3b6cfa04
Francent Jienarta <francentjie@gmail.com>
2026-02-16 18:41:10 +0700

    week02: (task 2) create Hero class

--- src/oop_00000115728_francentjienarta/week02/Hero.kt
+++ src/oop_00000115728_francentjienarta/week02/Hero.kt
@@ -1,2 +1,20 @@
 package oop_00000115728_francentjienarta.week02
 
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+    fun attack(targetName: String) {
+        println("$name attacked $targetName!")
+    }
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
