3c1a39569616060aaf840d04a33a1b6cbda5f82e
Francent Jienarta <francentjie@gmail.com>
2026-02-16 18:39:27 +0700

    week02: (task 1) implement library input system

--- src/oop_00000115728_francentjienarta/week02/MainLoan.kt
+++ src/oop_00000115728_francentjienarta/week02/MainLoan.kt
@@ -27,3 +27,9 @@ fun main(){
     println("Lama Pinjam    : ${loan.loanDuration} hari")
     println("Total Denda    : Rp. ${loan.calculateFine()}")
 }
+
+// Mohon maaf pak sebelumnya karena terlambat 7 hari
+// Sebelumnya saya sudah commit tepat waktu (Hari H)
+// Tapi karena saat mengerjakan tugas week 3 saya melakukan kesalahan
+// Tugas week2 jadi kena efeknya juga, beberapa commit jadi salah
+// Jadi saya commit ulang beberapa yang salah
