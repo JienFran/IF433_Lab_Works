391719274d952283424fce24a02b8215fac01c2e
Francent Jienarta <francentjie@gmail.com>
2026-02-23 19:48:02 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000115728_francentjienarta/week04/Main.kt
+++ src/oop_00000115728_francentjienarta/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val tesla = ElectricCar(brand = "Tesla", numberOfDoors = 4, batteryCapacity = 85)
+    tesla.accelerate()
+    tesla.honk()
+    tesla.openTrunk()
 }
