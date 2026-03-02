49bfdebcebae3c04a79e45f4277fe3dbb2ba4646
Francent Jienarta <francentjie@gmail.com>
2026-03-02 15:52:47 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000115728_francentjienarta/week05/task2/EWallet.kt
@@ -0,0 +1,17 @@
+package oop_00000115728_francentjienarta.week05.task2
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran sukses. Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo: $balance")
+    }
+}
