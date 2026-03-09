7a6609c890318bc540703a0c483e4528f83e7d52
Francent Jienarta <francentjie@gmail.com>
2026-03-09 19:51:45 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000115728_francentjienarta/week06/task1/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000115728_francentjienarta.week06.task1
+
+fun main() {
+    val lamp = SmartLamp("L001", "Ruang Tamu")
+    val speaker = SmartSpeaker("S002", "Google Nest Dapur")
+    val cctv = SmartCCTV("C007", "Ezviz Garasi")
+}
