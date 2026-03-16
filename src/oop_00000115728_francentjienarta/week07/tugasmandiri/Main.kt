9a8824d2bf92eb69b1baa8d9c029bb8f6470b28e
Francent Jienarta <francentjie@gmail.com>
2026-03-16 10:31:00 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000115728_francentjienarta/week07/tugasmandiri/Main.kt
+++ src/oop_00000115728_francentjienarta/week07/tugasmandiri/Main.kt
@@ -3,4 +3,13 @@ package oop_00000115728_francentjienarta.week07.tugasmandiri
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("Nilai drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Detail Starter Weapon:")
+    println("Nama: ${starterWeapon.item.name}")
+    println("Damage: ${starterWeapon.item.damage}")
+    println("Rarity: ${starterWeapon.item.rarity}")
+    println("Durability: ${starterWeapon.durability}")
 }
