1e55dba44be80885141f8d87aef085dd4872c1c9
Francent Jienarta <francentjie@gmail.com>
2026-03-16 09:30:56 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_00000115728_francentjienarta/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_00000115728_francentjienarta.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect () {
+        connectionStatus = "Connected to Server"
+        println("Databaser is ready.")
+    }
+}
