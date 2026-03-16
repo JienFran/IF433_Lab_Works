a6c768762852bba5a6d5dc6533ced060d5fd762f
Francent Jienarta <francentjie@gmail.com>
2026-03-16 10:20:20 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000115728_francentjienarta/week07/tugasmandiri/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000115728_francentjienarta.week07.tugasmandiri
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
