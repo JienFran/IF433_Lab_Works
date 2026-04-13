593125f33fb64e0335783632e14f47df1b4c0ac3
Francent Jienarta <francentjie@gmail.com>
2026-04-13 12:26:58 +0700

    week08: create nested classes for Order

---
+++ src/oop_00000115728_francentjienarta/week08/OrderModels.kt
@@ -0,0 +1,7 @@
+package oop_00000115728_francentjienarta.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
+
