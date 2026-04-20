3b4edd574f0159d46b731e562cb020859f6a6290
Francent Jienarta <francentjie@gmail.com>
2026-04-20 19:42:37 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000115728_francentjienarta/week09/tugasmandiri/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000115728_francentjienarta.week09.tugasmandiri
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 8.3, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 25, 12.1, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 5, -10.0, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 10, -2.5, "OPEN")
+    )
+}
