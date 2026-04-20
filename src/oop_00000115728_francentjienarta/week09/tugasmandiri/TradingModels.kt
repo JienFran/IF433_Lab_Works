f6457175e52797698ef95728126146d93ae648af
Francent Jienarta <francentjie@gmail.com>
2026-04-20 19:41:36 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000115728_francentjienarta/week09/tugasmandiri/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000115728_francentjienarta.week09.tugasmandiri
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
