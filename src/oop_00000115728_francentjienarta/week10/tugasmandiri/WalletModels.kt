e05b8e02227f2a36eb6932952d890ba36d082843
Francent Jienarta <francentjie@gmail.com>
2026-04-27 11:39:29 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000115728_francentjienarta/week10/tugasmandiri/WalletModels.kt
+++ src/oop_00000115728_francentjienarta/week10/tugasmandiri/WalletModels.kt
@@ -1,5 +1,8 @@
 package oop_00000115728_francentjienarta.week10.tugasmandiri
 
-data class Coin(val name: String, val balance: Double)
+data class Coin(
+    override val name: String,
+    val balance: Double
+) : NamedEntity
 
 data class Transaction(val id: String, val amount: Double)
--- src/oop_00000115728_francentjienarta/week10/tugasmandiri/WalletRepository.kt
+++ src/oop_00000115728_francentjienarta/week10/tugasmandiri/WalletRepository.kt
@@ -1,6 +1,10 @@
 package oop_00000115728_francentjienarta.week10.tugasmandiri
 
-class WalletRepository<T> {
+interface NamedEntity {
+    val name: String
+}
+
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
     fun add(item: T) {
         items.add(item)
@@ -8,5 +12,11 @@ class WalletRepository<T> {
 
     fun getAll(): List<T> {
         return items
+    }
+
+    fun searchByName(keyword: String): List<T> {
+        return items.filter {
+            it is NamedEntity && it.name.contains(keyword, ignoreCase = true)
+        }
     }
 }
