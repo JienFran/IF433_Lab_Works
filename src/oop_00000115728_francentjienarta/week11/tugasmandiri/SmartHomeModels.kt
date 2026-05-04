cb3611f535ca9d660860d6125017da13b998decf
Francent Jienarta <francentjie@gmail.com>
2026-05-04 20:25:25 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000115728_francentjienarta/week11/tugasmandiri/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000115728_francentjienarta.week11.tugasmandiri
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
