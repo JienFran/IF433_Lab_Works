3665538a58d7dbb96d693c0b085b888155ec0691
Francent Jienarta <francentjie@gmail.com>
2026-05-25 13:17:42 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000115728_francentjienarta/week14/tugasmandiri/EcommerceTask.kt
+++ src/oop_00000115728_francentjienarta/week14/tugasmandiri/EcommerceTask.kt
@@ -1,5 +1,6 @@
 package oop_00000115728_francentjienarta.week14.tugasmandiri
 import java.io.File
+import java.io.FileWriter
 
 interface OrderRepository {
     fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
@@ -8,7 +9,7 @@ interface OrderRepository {
 class CsvOrderRepository : OrderRepository {
     private val file = File("orders.csv")
     override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
-        file.bufferedWriter().use {
+        FileWriter(file, true).use {
             it.append("$itemName,$finalPrice,$customerType\n")
         }
     }
@@ -25,14 +26,43 @@ class EmailNotifier : NotificationService {
 }
 
 class SafeOrderProcessor(private val repo: OrderRepository, private val notifier: NotificationService) {
-    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        val finalPrice = when(customerType){
-            "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90
-            else -> basePrice
-        }
+    fun processOrder(
+        itemName: String,
+        basePrice: Double,
+        customerType: String,
+        pricingStrategy: PricingStrategy
+    ) {
+        val finalPrice = pricingStrategy.calculate(basePrice)
         println("Memproses pesanan $itemName seharga $finalPrice")
         repo.saveOrder(itemName, finalPrice, customerType)
         notifier.sendNotification("Pesanan $itemName Anda telah dikonfirmasi!")
     }
+}
+
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double {
+        return price
+    }
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double {
+        return price * 0.90
+    }
+}
+
+fun main() {
+    val repository = CsvOrderRepository()
+    val notifier = EmailNotifier()
+    val processor = SafeOrderProcessor(repository, notifier)
+    processor.processOrder(
+        "Handphone",
+        3700000.0,
+        "VIP",
+        VipPricing()
+    )
 }
